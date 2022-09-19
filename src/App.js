e059228580ecaf586bf84ff325723b4f943f0b92
Vanessa Lamarque <lamarque.van@gmail.com>
2022-09-19 12:26:38 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Button from 'react-bootstrap/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App d-flex justify-content-center">
+			<div className="text-container d-flex flex-column align-items-center mt-4 p-5">
+				<h1 className="display-1">Under Construction</h1>
+				<h2>Please check back soon!</h2>
+				<div className="d-flex flex-column align-items-center mt-4">
+					<p>Want to reach me in the meantime?</p>
+					<a
+						target="_blank"
+						href="mailto:vanessa@vanessalamarque.com"
+						rel="noreferrer"
+					>
+						<Button variant="dark py-2 px-4">Let's chat!</Button>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
