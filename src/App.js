5e93bc14a17d3ca2b14f6cadd5b8bc1e8d824a30
Vanessa Lamarque <lamarque.van@gmail.com>
2022-09-20 22:47:29 -0400

    started building UI (navbar, hero, portfolio)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css'
-import Button from 'react-bootstrap/Button'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Portfolio from './components/Portfolio/Portfolio'
 
 function App() {
 	return (
-		<div className="App d-flex justify-content-center">
-			<div className="text-container d-flex flex-column align-items-center mt-4 p-5">
-				<h1 className="display-1">Under Construction</h1>
-				<h2>Please check back soon!</h2>
-				<div className="d-flex flex-column align-items-center mt-4">
-					<p>Want to reach me in the meantime?</p>
-					<a
-						target="_blank"
-						href="mailto:vanessa@vanessalamarque.com"
-						rel="noreferrer"
-					>
-						<Button variant="dark py-2 px-4">Let's chat!</Button>
-					</a>
-				</div>
-			</div>
+		<div className="App">
+			<Header />
+			<Hero />
+			<Portfolio />
 		</div>
 	)
 }
